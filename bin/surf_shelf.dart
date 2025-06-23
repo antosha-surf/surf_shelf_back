7ac39322c10dcd979f8f3ee850bf9a11b754a12b
antosha <nazarov@surf.dev>
2025-06-23 12:51:13 +0500

    add ws connections

--- bin/surf_shelf.dart
+++ bin/surf_shelf.dart
@@ -2,12 +2,14 @@ import 'package:shelf/shelf.dart';
 import 'package:shelf_router/shelf_router.dart';
 import 'package:shelf/shelf_io.dart' as shelf_io;
 import 'package:surf_shelf/services/services.dart';
+import 'package:surf_shelf/services/ws_counter_service/ws_counter_service.dart';
 
 void main(List<String> arguments) async {
   final router = Router();
 
   final services = <Service>[
     TodosService(),
+    WSCounterService(),
     //
     // ... add your services here ...
     //
